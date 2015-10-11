c97e8baffa0b3f9ed25c9c0170bf85bc9767ba64
Alex Tamoykin <a.tamoykin@gmail.com>
2015-10-10 21:51:46 -0700

    Adds youtubeLabel and youtubeUsernameField

--- ScoutReport/ChooseYoutubeViewController.swift
+++ ScoutReport/ChooseYoutubeViewController.swift
@@ -1,4 +1,11 @@
 import UIKit
 
 class ChooseYoutubeViewController: UIViewController {
+    @IBOutlet var youtubeLabel: UILabel!
+    @IBOutlet var youtubeUsernameField: UITextField!
+
+    override func viewDidLoad() {
+        youtubeLabel.font = Config.standardHeaderFont
+        youtubeUsernameField.font = Config.standardBodyFont
+    }
 }
---
+++ ScoutReportTests/ChooseYoutubeViewControllerTest.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import ScoutReport
+
+class ChooseYoutubeViewControllerTest: XCTestCase {
+    var controller: ChooseYoutubeViewController!
+
+    override func setUp() {
+        controller = UIStoryboard.loadViewController(storyboardName: "ChooseSuspect", identifier: "chooseYoutubeViewController") as! ChooseYoutubeViewController
+        controller.loadViewIfNeeded()
+    }
+
+    func testAppearance() {
+        XCTAssertEqual(Config.standardHeaderFont, controller.youtubeLabel.font)
+        XCTAssertEqual(Config.standardBodyFont, controller.youtubeUsernameField.font)
+    }
+}
